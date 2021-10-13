d1437ce51a7334b5dd22c7335392190d3123c5b0
Kishan Kumar <kishan.kumar94173@gmail.com>
2021-10-13 14:19:39 +0530

    Initial commit

---
+++ force-app/main/default/lwc/pdfLwc/pdfLwc.js
@@ -0,0 +1,23 @@
+import { api, LightningElement } from 'lwc';
+import getApexData from '@salesforce/apex/lwcPdfController.generatePDF';
+
+export default class PdfLwc extends LightningElement {
+
+
+@api recordId;
+    generatePdf(){
+
+        /*var url = '/apex/envelope';
+        window.open(url);*/  // this will open the pdf in a new Window where user can download it
+       
+        getApexData({ recordId })
+          .then(result => {
+            console.log('Result', result);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+        });
+    
+    }
+
+}
